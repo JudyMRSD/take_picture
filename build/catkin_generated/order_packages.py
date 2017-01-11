ebbdc1f09462e65eb16494a689674b000ebe4b8f
jin <jinz1@andrew.cmu.edu>
2017-01-11 14:47:34 -0500

    added msg and srv folders in beginner_tutorials

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/jin/ros/take_picture_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/jin/common_ws/devel;/opt/ros/indigo".split(';') if "/home/jin/common_ws/devel;/opt/ros/indigo" != "" else []
+underlay_workspaces = "/home/jin/ros/take_picture_ws/devel;/home/jin/common_ws/devel;/opt/ros/indigo".split(';') if "/home/jin/ros/take_picture_ws/devel;/home/jin/common_ws/devel;/opt/ros/indigo" != "" else []
